993aaaf44b0dabec32069a203027f2ef90ea2d2f
Russell McOrmond <rmcormond@crkn.ca>
2021-12-14 12:41:41 -0500

    Demonstration tool uses write_file option for downloading larger files. Also needs a larger timeout. Perl tidy #20

--- bin/swift-download.pl
+++ bin/swift-download.pl
@@ -10,41 +10,54 @@ use DateTime::Format::ISO8601;
 
 my %options;
 
-GetOptions (\%options, 'server:s', 'user:s','password:s','account:s','verbose');
+GetOptions( \%options, 'server:s', 'user:s', 'password:s', 'account:s',
+    'verbose' );
 my $verbose = $options{'verbose'};
 delete $options{verbose};
 
-if (scalar(@ARGV) != 3) {
+# Longer timeout needed than default.
+$options{furl_options} = { timeout => 3600 };
 
-    print STDERR "Usage: --server <url> --user <username> --password <password> <container name> <object name> <disk filename>\n";
+if ( scalar(@ARGV) != 3 ) {
+
+    print STDERR
+"Usage: --server <url> --user <username> --password <password> <container name> <object name> <disk filename>\n";
 }
-my ($container,$object,$filename) = @ARGV;
-
+my ( $container, $object, $filename ) = @ARGV;
 
 my $swift = CIHM::Swift::Client->new(%options);
 
 if ($swift) {
-    my $response = $swift->object_get($container,$object);
-    if ($response->code == 200) {
-	open(my $fh, '>:raw', $filename)
-	    or die "Could not open file '$filename' $!";
-	print $fh $response->content;
-	close $fh;
-	my $filemodified = $response->object_meta_header('File-Modified');
-	if ($filemodified) {
-	    print "modified: $filemodified\n" if ($verbose);
+    open( my $fh, '>:raw', $filename )
+      or die "Could not open file '$filename' $!";
+    print "open ($filename) to write\n" if ($verbose);
+    my $response =
+      $swift->object_get( $container, $object, { write_file => $fh } );
+    close $fh;
+    if ( $response->code == 200 ) {
+        my $filemodified = $response->object_meta_header('File-Modified');
+        if ($filemodified) {
+            print "modified: $filemodified\n" if ($verbose);
 
-	    my $dt = DateTime::Format::ISO8601->parse_datetime( $filemodified );
-	    if (! $dt) {
-		die "Couldn't parse ISO8601 date from $filemodified\n";
-	    }
-	    my $atime=time;
-	    utime $atime, $dt->epoch(), $filename;
-	}
-	print "Headers from stored object:\n". $response->headers->as_string()."\n" if ($verbose);
-    } else {
-	print STDERR "account: '".$swift->account."' , container: '$container' , object: '$object'  returned ". $response->code . " - " . $response->message. "\n";
+            my $dt = DateTime::Format::ISO8601->parse_datetime($filemodified);
+            if ( !$dt ) {
+                die "Couldn't parse ISO8601 date from $filemodified\n";
+            }
+            my $atime = time;
+            utime $atime, $dt->epoch(), $filename;
+        }
+        print "Headers from stored object:\n"
+          . $response->headers->as_string() . "\n"
+          if ($verbose);
     }
-} else {
+    else {
+        print STDERR "account: '"
+          . $swift->account
+          . "' , container: '$container' , object: '$object'  returned "
+          . $response->code . " - "
+          . $response->message . "\n";
+    }
+}
+else {
     print STDERR "No CIHM::Swift::Client()\n";
 }
