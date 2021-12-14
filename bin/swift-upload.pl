6074994b0e75dce8cdaee5627a8ffbb8bb0c5880
Russell McOrmond <rmcormond@crkn.ca>
2021-12-14 15:40:06 -0500

    Perltidy, increase timeout to handle larger files, display response code and message.

--- bin/swift-upload.pl
+++ bin/swift-upload.pl
@@ -9,37 +9,54 @@ use DateTime;
 
 my %options;
 
-GetOptions (\%options, 'server:s', 'user:s','password:s','account:s','verbose');
+GetOptions( \%options, 'server:s', 'user:s', 'password:s', 'account:s',
+    'verbose' );
 my $verbose = $options{'verbose'};
 delete $options{verbose};
 
-if (scalar(@ARGV) != 3) {
+# Longer timeout needed than default.
+$options{furl_options} = { timeout => 3600 };
 
-    print STDERR "Usage: --server <url> --user <username> --password <password> <container name> <object name> <disk filename>\n";
+if ( scalar(@ARGV) != 3 ) {
+
+    print STDERR
+"Usage: --server <url> --user <username> --password <password> <container name> <object name> <disk filename>\n";
 }
-my ($container,$object,$filename) = @ARGV;
-
+my ( $container, $object, $filename ) = @ARGV;
 
 my $swift = CIHM::Swift::Client->new(%options);
 
 if ($swift) {
-    open(my $fh, '<:raw', $filename)
-	or die "Could not open file '$filename' $!";
+    open( my $fh, '<:raw', $filename )
+      or die "Could not open file '$filename' $!";
 
-    my $filedate="unknown";
-    my $mtime=(stat($fh))[9];
+    my $filedate = "unknown";
+    my $mtime    = ( stat($fh) )[9];
     if ($mtime) {
-	my $dt = DateTime->from_epoch(epoch => $mtime);
-        $filedate = $dt->datetime. "Z";
+        my $dt = DateTime->from_epoch( epoch => $mtime );
+        $filedate = $dt->datetime . "Z";
     }
     print "File modified date: $filedate\n" if ($verbose);
-    $swift->object_put($container,$object, $fh, { 'File-Modified' => $filedate});
+    my $response = $swift->object_put( $container, $object, $fh,
+        { 'File-Modified' => $filedate } );
     close $fh;
 
-    if ($verbose) {
-	# Separate request to confirm headers   
-	print "Headers from stored object:\n". $swift->object_head($container,$object)->headers->as_string()."\n";
+    if ( $response->code != 201 ) {
+        print STDERR "account: '"
+          . $swift->account
+          . "' , container: '$container' , object: '$object'  returned "
+          . $response->code . " - "
+          . $response->message . "\n";
     }
-} else {
+
+    if ($verbose) {
+
+        # Separate request to confirm headers
+        print "Headers from stored object:\n"
+          . $swift->object_head( $container, $object )->headers->as_string()
+          . "\n";
+    }
+}
+else {
     print STDERR "No CIHM::Swift::Client()\n";
 }
